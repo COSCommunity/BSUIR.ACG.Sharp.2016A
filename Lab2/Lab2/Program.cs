28d9a547c659abe70464e90177e1836dcd1781bd
Artsiom_Prakapovich <Artsiom_Prakapovich@EPBYMINW6631>
2016-11-14 11:02:27 +0300

    lab2 recovered

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -30,54 +30,69 @@ namespace Lab2
                     switch (sdlEvent.type)
                     {
                         case SDL.SDL_EventType.SDL_QUIT:
-                            {
-                                quit = true;
-                                break;
-                            }
+                        {
+                            quit = true;
+                            break;
+                        }
                         case SDL.SDL_EventType.SDL_KEYDOWN:
+                        {
+                            var key = sdlEvent.key;
+                            switch (key.keysym.sym)
                             {
-                                var key = sdlEvent.key;
-                                switch (key.keysym.sym)
-                                {
-                                    case SDL.SDL_Keycode.SDLK_DOWN:
-                                        shape.TransformY += 10;
-                                        break;
-                                    case SDL.SDL_Keycode.SDLK_UP:
-                                        shape.TransformY -= 10;
-                                        break;
-                                    case SDL.SDL_Keycode.SDLK_LEFT:
-                                        shape.TransformX -= 10;
-                                        break;
-                                    case SDL.SDL_Keycode.SDLK_RIGHT:
-                                        shape.TransformX += 10;
-                                        break;
-                                    case SDL.SDL_Keycode.SDLK_LCTRL:
-                                        shape.Rotate += 10;
-                                        break;
-                                    case SDL.SDL_Keycode.SDLK_RCTRL:
-                                        shape.Rotate -= 10;
-                                        break;
-                                }
-                                break;
+                                case SDL.SDL_Keycode.SDLK_DOWN:
+                                    shape.TransformY += 10;
+                                    break;
+                                case SDL.SDL_Keycode.SDLK_UP:
+                                    shape.TransformY -= 10;
+                                    break;
+                                case SDL.SDL_Keycode.SDLK_LEFT:
+                                    shape.TransformX -= 10;
+                                    break;
+                                case SDL.SDL_Keycode.SDLK_RIGHT:
+                                    shape.TransformX += 10;
+                                    break;
+                                case SDL.SDL_Keycode.SDLK_LCTRL:
+                                    shape.Rotate += 10;
+                                    break;
+                                case SDL.SDL_Keycode.SDLK_RCTRL:
+                                    shape.Rotate -= 10;
+                                    break;
+                                case SDL.SDL_Keycode.SDLK_9:
+                                    shape.U = shape.U < 0.9 ? shape.U + 0.05 : shape.U;
+                                    break;
+                                case SDL.SDL_Keycode.SDLK_0:
+                                    shape.U = shape.U > 0.1 ? shape.U - 0.05 : shape.U;
+                                    break;
+                                case SDL.SDL_Keycode.SDLK_1:
+                                    shape.Angle = shape.Angle < 10 ? shape.Angle + 1 : shape.Angle;
+                                    break;
+                                case SDL.SDL_Keycode.SDLK_2:
+                                    shape.Angle = shape.Angle > 3 ? shape.Angle - 1 : shape.Angle;
+                                    break;
+                                case SDL.SDL_Keycode.SDLK_TAB:
+                                    shape.Mode = !shape.Mode;
+                                    break;
                             }
+                            break;
+                        }
                         case SDL.SDL_EventType.SDL_MOUSEBUTTONDOWN:
+                        {
+                            if (sdlEvent.button.button == SDL.SDL_BUTTON_LEFT)
                             {
-                                if (sdlEvent.button.button == SDL.SDL_BUTTON_LEFT)
+                                if (shape.Scale < 3)
                                 {
-                                    if (shape.Scale < 3)
-                                    {
-                                        shape.Scale += 0.1;
-                                    }
+                                    shape.Scale += 0.1;
                                 }
-                                if (sdlEvent.button.button == SDL.SDL_BUTTON_RIGHT)
-                                {
-                                    if (shape.Scale > 0.2)
-                                    {
-                                        shape.Scale -= 0.1;
-                                    }
-                                }
-                                break;
                             }
+                            if (sdlEvent.button.button == SDL.SDL_BUTTON_RIGHT)
+                            {
+                                if (shape.Scale > 0.2)
+                                {
+                                    shape.Scale -= 0.1;
+                                }
+                            }
+                            break;
+                        }
 
                     }
                     DrawShape(renderer, shape);
@@ -95,9 +110,43 @@ namespace Lab2
         {
             SDL.SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
             SDL.SDL_RenderClear(renderer);
+
             SDL.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-
-
+            var points = shape.Points;
+            if (shape.Mode)
+            {
+                for (int j = 0; j < points.Length; j++)
+                {
+                    if (j%2 == 0)
+                    {
+                        var temp = points[j].ToList();
+                        temp.Add(points[j][0]);
+                        for (int i = 0; i < points[j].Length; i++)
+                        {
+                            DrawLine(renderer, temp[i], temp[i + 1]);
+                        }
+                    }
+                    else
+                    {
+                        DrawCircle(renderer, points[j][0].x, points[j][0].y,
+                            Convert.ToInt32(shape.Radiuses[j]*shape.Scale));
+                    }
+                    //SDL.SDL_RenderDrawLines(renderer, temp.ToArray(), arr.Length + 1);
+                }
+            }
+            else
+            {
+                for (int j = 0; j < points.Length; j++)
+                {
+                    var temp = points[j].ToList();
+                    temp.Add(points[j][0]);
+                    for (int i = 0; i < points[j].Length; i++)
+                    {
+                        DrawLine(renderer, temp[i], temp[i + 1]);
+                    }
+                    //SDL.SDL_RenderDrawLines(renderer, temp.ToArray(), arr.Length + 1);
+                }
+            }
 
             SDL.SDL_RenderPresent(renderer);
         }
@@ -150,129 +199,161 @@ namespace Lab2
 
         private static void DrawCircle(IntPtr renderer, int _x, int _y, int radius)
         {
-            int x = 0, y = radius, gap = 0, delta = (2 - 2 * radius);
+            int x = 0, y = radius, gap = 0, delta = (2 - 2*radius);
             while (y >= 0)
             {
                 SDL.SDL_RenderDrawPoint(renderer, _x + x, _y + y);
                 SDL.SDL_RenderDrawPoint(renderer, _x + x, _y - y);
                 SDL.SDL_RenderDrawPoint(renderer, _x - x, _y - y);
                 SDL.SDL_RenderDrawPoint(renderer, _x - x, _y + y);
-                gap = 2 * (delta + y) - 1;
+                gap = 2*(delta + y) - 1;
                 if (delta < 0 && gap <= 0)
                 {
                     x++;
-                    delta += 2 * x + 1;
+                    delta += 2*x + 1;
                     continue;
                 }
                 if (delta > 0 && gap > 0)
                 {
                     y--;
-                    delta -= 2 * y + 1;
+                    delta -= 2*y + 1;
                     continue;
                 }
                 x++;
-                delta += 2 * (x - y);
+                delta += 2*(x - y);
                 y--;
             }
         }
 
-        private abstract class Shape
+        private class Shape
         {
-            public int Rotate
+            private const int InnerShapeCount = 10;
+            private int Radius = 100;
+
+            private bool mode;
+
+            public bool Mode
             {
-                get { return transformer.Rotate; }
-                set { transformer.Rotate = value; }
-            }
-
-            public double Scale
-            {
-                get { return transformer.Scale; }
-                set { transformer.Scale = value; }
-            }
-
-            public int TransformX
-            {
-                get { return transformer.TransformX; }
-                set { transformer.TransformX = value; }
-            }
-
-            public int TransformY
-            {
-                get { return transformer.TransformY; }
-                set { transformer.TransformY = value; }
-            }
-
-            public int Layer { get; set; }
-            protected abstract SDL.SDL_Point[] Points { get; }
-
-            private readonly PointTransformer transformer = new PointTransformer()
-            {
-                Rotate = 0,
-                Scale = 1,
-                TransformX = 300,
-                TransformY = 300
-            };
-            public SDL.SDL_Point[] GetShapePoints()
-            {
-                return transformer.TransformPoints(Points);
-            }
-        }
-
-        private class Polygon : Shape
-        {
-            public Polygon(int layer, SDL.SDL_Point[] points)
-            {
-                this.Layer = layer;
-                this.Points = points;
-            }
-
-            protected override SDL.SDL_Point[] Points { get; }
-        }
-
-        private class Ellipse : Shape
-        {
-            public Ellipse(int layer, int A, int B)
-            {
-                this.Layer = layer;
-                this.Points = InitPoints(A, B);
-            }
-
-            protected override SDL.SDL_Point[] Points { get; }
-
-            private SDL.SDL_Point[] InitPoints(int A, int B)
-            {
-                const int PointNum = 10000;
-                var result = new SDL.SDL_Point[PointNum];
-                for (int i = 0; i < result.Length; i++)
+                get { return mode; }
+                set
                 {
-                    result[i] = new SDL.SDL_Point
-                    {
-                        x = Convert.ToInt32(A*Math.Cos(2*Math.PI/PointNum*i)),
-                        y = Convert.ToInt32(B*Math.Sin(2*Math.PI/PointNum*i))
-                    };
+                    mode = value;
+                    points = mode ? InitCircleShape() : InitShape();
                 }
-                return result;
             }
-        }
 
-        private class PointTransformer
-        {
-            public double Scale { get; set; }
+            private int angle;
+
+            public int Angle
+            {
+                get { return angle; }
+                set
+                {
+                    angle = value;
+                    points = InitShape();
+                }
+            }
+
+            private double u;
+
+            public double U
+            {
+                get { return u; }
+                set
+                {
+                    u = value;
+                    points = InitShape();
+                }
+            }
+
             public int Rotate { get; set; }
+            public double Scale { get; set; }
             public int TransformX { get; set; }
             public int TransformY { get; set; }
 
-            public SDL.SDL_Point[] TransformPoints(SDL.SDL_Point[] points)
+            private SDL.SDL_Point[][] points;
+            public SDL.SDL_Point[][] Points => TransformPoints();
+            public int[] Radiuses = new int[InnerShapeCount];
+
+            public Shape(int angles = 4)
+            {
+                u = 0.2;
+                Scale = 1;
+                Rotate = 0;
+                TransformX = 300;
+                TransformY = 300;
+                angle = angles;
+                points = InitShape();
+            }
+
+            private SDL.SDL_Point[][] InitCircleShape()
+            {
+                var result = new SDL.SDL_Point[InnerShapeCount][];
+                var step = 2*Math.PI/4;
+                double currentRadius = Radius;
+                for (int i = 0; i < InnerShapeCount; i++)
+                {
+                    result[i] = new SDL.SDL_Point[4];
+                    if (i%2 == 0)
+                    {
+                        for (int j = 0; j < 4; j++)
+                        {
+                            result[i][j].x = Convert.ToInt32(currentRadius*Math.Cos(step*j));
+                            result[i][j].y = Convert.ToInt32(currentRadius*Math.Sin(step*j));
+                        }
+                        currentRadius = currentRadius/Math.Sqrt(2);
+                    }
+                    else
+                    {
+                        result[i][0].x = 0;
+                        result[i][0].y = 0;
+                        Radiuses[i] = Convert.ToInt32(currentRadius);
+                    }
+                }
+                return result;
+            }
+
+            private SDL.SDL_Point[][] InitShape()
+            {
+                var result = new SDL.SDL_Point[InnerShapeCount][];
+                var step = 2*Math.PI/Angle;
+                for (int i = 0; i < InnerShapeCount; i++)
+                {
+                    result[i] = new SDL.SDL_Point[Angle];
+                    for (int j = 0; j < Angle; j++)
+                    {
+                        if (i == 0)
+                        {
+                            result[i][j].x = Convert.ToInt32(Radius*Math.Cos(step*j));
+                            result[i][j].y = Convert.ToInt32(Radius*Math.Sin(step*j));
+                        }
+                        else
+                        {
+                            result[i][j].x =
+                                Convert.ToInt32((1 - U)*result[i - 1][j].x + U*result[i - 1][(j + 1)%Angle].x);
+                            result[i][j].y =
+                                Convert.ToInt32((1 - U)*result[i - 1][j].y + U*result[i - 1][(j + 1)%Angle].y);
+                        }
+                    }
+                }
+                return result;
+            }
+
+            private SDL.SDL_Point[][] TransformPoints()
             {
                 var scale = Matrix.ScaleMatrix(Scale);
                 var rotate = Matrix.RotateMatrix(Rotate);
                 var transform = Matrix.TransformMatrix(TransformX, TransformY);
                 var srtMatrix = transform*rotate*scale;
-                var result = new SDL.SDL_Point[points.Length];
+                var result = new SDL.SDL_Point[points.Length][];
                 for (int i = 0; i < points.Length; i++)
                 {
-                    var vector = Matrix.VectorMatrix(points[i]);
-                    result[i] = (SDL.SDL_Point) (srtMatrix*vector);
+                    result[i] = new SDL.SDL_Point[points[i].Length];
+                    for (int j = 0; j < result[i].Length; j++)
+                    {
+                        var vector = Matrix.VectorMatrix(points[i][j]);
+                        result[i][j] = (SDL.SDL_Point) (srtMatrix*vector);
+                    }
                 }
                 return result;
             }
